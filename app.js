dc791fa19f892800bb5c1316fa7ffbdc3f57dc38
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-06-01 14:23:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ bot.on("ready", async () => {
 });
 
 bot.on(`guildMemberAdd`, member => {
-  let welChannel = message.guild.channels.find(`LateZ Group`, "welcome");
+  let welChannel = member.guild.channels.find(`LateZ Group`, "welcome");
 
 
   welChannel.send(`<@${member.user.id}>, **Tervetuloa!**`);
